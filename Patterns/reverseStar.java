1e1137a6c3a4f3bc0140c5e1dfb48fbd5127c2c9
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-01-01 14:57:38 +0530

    Update reverseStar.java

--- Patterns/reverseStar.java
+++ Patterns/reverseStar.java
@@ -1,5 +1,9 @@
 public class reverseStar {
     public static void main(String[] args) {
+        // ****
+        // ***
+        // **
+        // *
         int n = 4;
         for(int line = 1;line <= n; line++) {
             for(int star = 1; star <= n-line+1; star++) {
