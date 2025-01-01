969aa13977301d290816dfd2dd635c556289ea70
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-01-01 22:33:40 +0530

    Update secondLarge.java

--- Arrays/secondLarge.java
+++ Arrays/secondLarge.java
@@ -1,23 +1,23 @@
+/* Example :
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+*/
+
 public class secondLarge {
   public static int getSecond(int[] arr) {
     int n = arr.length;
     int largest = -1;
     int secondLargest = -1;
-
-    //find largest element
+    
     for(int i=0;i<n;i++) {
-    if(arr[i] > largest) {
-      largest = arr[i];
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] < largest) {
+            secondLargest = arr[i]; 
+        } 
     }
-  }
-
-    //find second largest
-    for(int i=0;i<n;i++) {
-      if(arr[i] > secondLargest && arr[i] != largest) {
-        secondLargest = arr[i];
-     }
-    }
-
     return secondLargest;
   }
     
