fb44ff3254f63a3a318bf647468c349221d1b78c
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-01-02 20:35:06 +0530

    Update largestElement.java

--- Arrays/largestElement.java
+++ Arrays/largestElement.java
@@ -1,16 +1,36 @@
 /* Example 1:
 Input: nums = [3,2,1,5,6,4], k = 2
 Output: 6 */
- 
+import java.util.*;
+
 public class largestElement {
+ //using priorityQueue
+ public static int kthelement(int nums[], int k) {
+  PriorityQueue<Integer> pq = new PriorityQueue<>();
+  for(int i=0;i<k;i++) {
+   pq.add(nums[i]);
+  }
+  for(int i=k;i<nums.length;i++) {
+   if(pq.peek() < nums[i]) {
+    pq.poll();
+    pq.add(nums[i]);
+   }
+  }
+  return pq.peek();
+ }
+
+ /*
+ //using arrays in built method
   public static int large(int[] nums, int k) {
     Arrays.sort(nums);
     return nums[nums.length-k];
-  }
+  } */
   public static void main(String[] args) {
     int nums[]= {3,2,1,5,6,4};
     int k = 1;
 
-    System.out.println(large(nums,k));
+   int result = kthelement(nums, k);
+   System.out.println(result);
+    //System.out.println(large(nums,k));
   }
 }
