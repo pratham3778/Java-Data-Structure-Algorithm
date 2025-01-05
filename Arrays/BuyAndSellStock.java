152ff8abe598207eb8348939c4ae7c97f498eea0
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-01-05 17:40:24 +0530

    Create BuyAndSellStock.java

---
+++ Arrays/BuyAndSellStock.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class BuyAndSellStock {
+public static int maxProfit(int[] prices) {
+        int minProfit = prices[0];
+        int maxProfit = 0;
+        
+        for(int i=1;i<prices.length;i++) {
+            int cost = prices[i] - minProfit;
+            maxProfit = Math.max(maxProfit, cost);
+            minProfit = Math.min(minProfit, prices[i]);
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int result = maxProfit(prices);
+        
+        System.out.println(result);
+    }
+}
