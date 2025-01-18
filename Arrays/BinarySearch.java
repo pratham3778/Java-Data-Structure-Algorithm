473e65bf93ed845ddda1e3c1c55546c8a4c86d5d
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-01-18 16:50:05 +0530

    Create BinarySearch.java

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1; 
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        BinarySearch bs = new BinarySearch();
+
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        int result = bs.search(nums, target);
+        System.out.println("found at index : " + result);
+    }
+}
