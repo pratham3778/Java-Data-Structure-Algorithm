b96e0497efee0dff85e2931fe6cd43451916e9f4
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-01-31 21:27:53 +0530

    Create ReverseWordsInString.java

---
+++ Strings/ReverseWordsInString.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class ReverseWordsInString {
+
+    public String reverseWords(String s) {
+        String[] words = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (words[i].length() > 0) {
+                sb.append(words[i]);
+                sb.append(' ');
+            }
+        }
+        return sb.substring(0, sb.length() - 1).toString();
+    }
+
+    public static void main(String[] args) {
+        ReverseWordsInString s = new ReverseWordsInString();
+        String input = "  the sky is blue  ";
+        String result = s.reverseWords(input);
+        System.out.println("Input: \"" + input + "\"");
+        System.out.println("Output: \"" + result + "\"");
+    }
+}
