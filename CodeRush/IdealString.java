adedc91e2596d9ce32e3cbed7b2acc0a3741bc5c
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-03-25 16:13:05 +0530

    Update IdealString.java

--- CodeRush/IdealString.java
+++ CodeRush/IdealString.java
@@ -1,22 +1,28 @@
 package CodeRush;
 
-// Find the return minimum number of removals to achieve ideal string
+// Find the return minimum number of removals and also ideal string
 // Input : XYXXYXXY
 // output : 2
 
 public class IdealString {
-    public static int minRemoval(String input) {
+    public static String minRemoval(String input) {
+        StringBuilder sb = new StringBuilder();
         int removal = 0;
 
+        sb.append(input.charAt(0));
+
         for(int i=1;i<input.length();i++) {
-            if(input.charAt(i) == input.charAt(i-1)) {
+            if(input.charAt(i) != input.charAt(i-1)) {
+                sb.append(input.charAt(i));
+            } else {
                 removal++;
             }
         }
-        return removal;
+        System.out.println("Minimum Removals: " + removal);
+        return sb.toString();
     }
     public static void main(String[] args) {
         String input = "XYXXYXXY";
-        System.out.println("Minimum Removals: " + minRemoval(input));
+        System.out.println("Ideal String: " + minRemoval(input));
     }
 }
