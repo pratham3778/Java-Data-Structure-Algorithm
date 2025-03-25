0939dd28a05a2fc02a19a2dec25013c519ddec20
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-03-25 16:09:09 +0530

    Create IdealString.java

---
+++ CodeRush/IdealString.java
@@ -0,0 +1,22 @@
+package CodeRush;
+
+// Find the return minimum number of removals to achieve ideal string
+// Input : XYXXYXXY
+// output : 2
+
+public class IdealString {
+    public static int minRemoval(String input) {
+        int removal = 0;
+
+        for(int i=1;i<input.length();i++) {
+            if(input.charAt(i) == input.charAt(i-1)) {
+                removal++;
+            }
+        }
+        return removal;
+    }
+    public static void main(String[] args) {
+        String input = "XYXXYXXY";
+        System.out.println("Output: " + minRemoval(input));
+    }
+}
