968beb81808cd33511bacea6f6395495ff728e28
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-03-31 12:05:13 +0530

    Create NumberOfSegmentsInString.java

---
+++ CodeRush/NumberOfSegmentsInString.java
@@ -0,0 +1,30 @@
+package CodeRush;
+
+public class NumberOfSegmentsInString {
+    public int countSegments(String s) {
+        // Split the string into words using spaces as separators
+        String[] words = s.split(" ");
+        int count = 0;
+
+        // Count non-empty words
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        NumberOfSegmentsInString ans = new NumberOfSegmentsInString();
+
+        String s1 = "Hello, my name is John";
+        System.out.println("Input: \"" + s1 + "\"");
+        System.out.println("Output: " + ans.countSegments(s1)); // Expected Output: 5
+
+        String s2 = "   "; 
+        System.out.println("Input: \"" + s2 + "\"");
+        System.out.println("Output: " + ans.countSegments(s2));
+    }
+}
