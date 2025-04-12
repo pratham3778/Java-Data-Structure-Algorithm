e71826053788bcbd7736dd928728eccfdda1c7cb
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-04-12 14:06:57 +0530

    Add files via upload

---
+++ CollectionFramework/Comparable.java
@@ -0,0 +1,19 @@
+package CollectionFramework;
+// Comparable is a default natural sorting order
+// String class and all wrapper class already implements comparable interface
+// present in java.lang package
+// contains one method - compareTo(T);
+// eg : Obj1.compareTo(Obj2)
+// return -ve if Obj1 has come before Obj2
+// return +ve if Obj1 has come after Obj2
+// return 0 if Obj1 and Obj2 are equals
+
+
+public class Comparable {
+
+    public static void main(String[] args) {
+        System.out.println("A".compareTo("B"));
+        System.out.println("B".compareTo("A"));
+        System.out.println("A".compareTo("A"));
+    }
+}
---
+++ CollectionFramework/Compartor.java
@@ -0,0 +1,53 @@
+package CollectionFramework;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+// Comparator interface is present in java.util package
+// we can use comparator interface to define our own customized order
+// compare(Obj1, Obj2);
+// return -ve if Obj1 has come before Obj2
+// return +ve if Obj1 has come after Obj2
+// return 0 if Obj1 and Obj2 are equals
+
+public class Compartor {
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = (i, j) -> Integer.compare(i, j);
+            //Integer.compare(j, i); 
+            //other way to write
+            //return i.compareTo(j); //return ASC
+            //return j.compareTo(i); //return DESC
+
+        TreeSet<Integer> treeSet = new TreeSet<>(comparator);
+        treeSet.add(30);
+        treeSet.add(10);
+        treeSet.add(20);
+        treeSet.add(40);
+
+        System.out.println(treeSet);
+    }
+}
+
+
+/* //Q. Write a program to insert integer object into TreeSet where the sorting order is descending order. (using comparator)
+
+import java.util.*;
+
+class Compartor  {
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = (i, j) -> Integer.compare(j, i);
+        
+        
+        TreeSet<Integer> treeSet = new TreeSet<>(comparator);
+        
+        treeSet.add(30);
+        treeSet.add(20);
+        treeSet.add(50);
+        treeSet.add(10);
+        treeSet.add(40);
+        
+        
+        System.out.println(treeSet);
+    }
+}
+ */
