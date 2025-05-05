b72c97d5ea6c323ed5fcce8165cdff904b2e7209
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2025-05-05 08:15:27 +0530

    Create StaticMem.java

---
+++ CoreJava/StaticMem.java
@@ -0,0 +1,26 @@
+package CoreJava;
+
+// static keyword in java indicates that the field or method belong to class rather than an instance
+
+// static variable are class level variable shared among all instance of the clas
+
+// static method are that can be created without creating an instance of class
+
+// EXAMPLE : Show school name - shared by all students
+class demo {
+    static void print() {
+        System.out.println("Im a static method");
+    }
+}
+public class StaticMem {
+    //static keyword
+    static int count  = 0; //static variable 
+    static void display() {
+        System.out.println("I'm a static keyword" + " - " +count);
+    }
+    public static void main(String[] args) {
+        StaticMem.display(); 
+        demo.print(); // static method - no object creation
+    }
+    
+}
